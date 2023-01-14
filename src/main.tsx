10f8be05d20c12fa4b88dacf39cd619ff7de6e98
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-01-14 22:01:45 +0800

    调整基本配置

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { App } from "@/App";
 import "@/assets/styles/index.css";
 import "@/config/basicConfig";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
