88fd5b47d7a0492995ca99d85b091f20b72be932
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-01-16 02:50:28 +0800

    简单实现购物栏的操作

---
+++ src/components/cartBar/CartBar.tsx
@@ -0,0 +1,76 @@
+import { CartStateType } from "@/interface/typeDefine";
+import { useEffect, useState } from "react";
+import CartBarStyle from "./CartBar.module.scss";
+import { CartDetail } from "./cartDetail/CartDetail";
+
+export function CartBar(props: { cartState: CartStateType }): JSX.Element {
+  /**
+   * 遮罩层可见性
+   */
+  const [visibleState, setVisibleState] = useState<boolean>(false);
+  useEffect((): void => {
+    if (props.cartState.totalCount <= 0) {
+      setVisibleState(() => false);
+    }
+  }, [props.cartState.totalCount]);
+
+  return (
+    <>
+      <div
+        className={CartBarStyle["cart-bar"]}
+        onClick={(ev): void => {
+          ev.stopPropagation();
+          if (props.cartState.totalAmount > 0) {
+            setVisibleState((oldVal: boolean): boolean => !oldVal);
+          }
+        }}
+      >
+        <div className={CartBarStyle["icon-wrapper"]}>
+          <img
+            src="/icon_img/bag.png"
+            alt="..."
+            className={CartBarStyle["bag-size"]}
+          />
+          <span
+            // 动态拼接样式字符串, 如果购物车商品数量为 0 的情况下添加灰色背景样式
+            className={`${CartBarStyle["count-hint"]} ${
+              props.cartState.totalCount <= 0 ? CartBarStyle["when-zero"] : ""
+            }`.trim()}
+          >
+            {props.cartState.totalCount}
+          </span>
+        </div>
+
+        {props.cartState.totalCount > 0 ? (
+          <div className={CartBarStyle["cart-price"]}>
+            {props.cartState.totalAmount}
+          </div>
+        ) : (
+          <div className={CartBarStyle["un-selected"]}>当前未选择任何商品</div>
+        )}
+
+        <div className={CartBarStyle["confirm-btn-wrapper"]}>
+          {props.cartState.totalCount > 0 ? (
+            <button
+              className={CartBarStyle["confirm-btn"]}
+              onClick={(ev): void => {
+                ev.stopPropagation();
+              }}
+            >
+              去结算
+            </button>
+          ) : (
+            <button disabled className={CartBarStyle["disable-btn"]}>
+              未选择商品
+            </button>
+          )}
+        </div>
+      </div>
+
+      {/* 弹出遮罩 */}
+      {visibleState && props.cartState.totalCount > 0 ? (
+        <CartDetail setVisibleState={setVisibleState} />
+      ) : null}
+    </>
+  );
+}
---
+++ src/components/cartBar/cartDetail/CartDetail.tsx
@@ -0,0 +1,96 @@
+import { Backdrop } from "@/components/ui/backdrop/Backdrop";
+import { Counter } from "@/components/ui/counter/Counter";
+import { MealDataType } from "@/interface/typeDefine";
+import { CartContext } from "@/store/CartContext";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useContext } from "react";
+import CartDetailStyle from "./CartDetail.module.scss";
+import MealStyle from "@/components/mealList/meal/Meal.module.scss";
+/**
+ * 在图片加载失败的时候添加伪类进行替换显示
+ * @param ev 图片加载失败事件
+ */
+function setImgLoadingErrorHint(
+  ev: React.SyntheticEvent<HTMLImageElement, Event>
+): void {
+  ev.currentTarget.classList.add(MealStyle["when-error"]);
+}
+
+export function CartDetail(props: {
+  setVisibleState: React.Dispatch<React.SetStateAction<boolean>>;
+}): JSX.Element {
+  const cartCtx = useContext(CartContext);
+
+  return (
+    <Backdrop
+      onClick={(): void => {
+        props.setVisibleState((): boolean => false);
+      }}
+    >
+      <div
+        className={CartDetailStyle["cart-detail-wrapper"]}
+        onClick={(ev): void => {
+          // 阻止事件冒泡
+          ev.stopPropagation();
+        }}
+      >
+        {/* 头部信息 */}
+        <header className={CartDetailStyle["header-wrapper"]}>
+          <p className={CartDetailStyle["text-desc"]}>套餐详情</p>
+          <div
+            className={CartDetailStyle["clear-cart"]}
+            onClick={(): void => {
+              cartCtx.clearCart();
+            }}
+          >
+            <FontAwesomeIcon icon={faTrash} />
+            <span className={CartDetailStyle["clear-desc"]}>清空购物车</span>
+          </div>
+        </header>
+
+        {/* 列表 */}
+        <div className={CartDetailStyle["meals-wrapper"]}>
+          {cartCtx.cartState.items.map((item: MealDataType): JSX.Element => {
+            return (
+              <div className={MealStyle["meal-wrapper"]} key={item.id}>
+                {/* 商品图片 */}
+                <div>
+                  <img
+                    src={item.img}
+                    className={MealStyle["meal-img"]}
+                    alt="加载失败..."
+                    loading="lazy"
+                    decoding="async"
+                    onError={setImgLoadingErrorHint}
+                  />
+                </div>
+
+                {/* 商品描述 */}
+                <div className={MealStyle["text-wrapper"]}>
+                  <h1 className={MealStyle["meal-title"]}>{item.title}</h1>
+
+                  <div className={MealStyle["price-wrapper"]}>
+                    {/* 总价 */}
+                    <div className={MealStyle["meal-price"]}>{item.price}</div>
+
+                    {/* 计数器 */}
+                    <Counter
+                      displayNum={item.count}
+                      onDecrease={(): void => {
+                        cartCtx.setCartHandler(item, "delete");
+                      }}
+                      onIncrease={(): void => {
+                        cartCtx.setCartHandler(item, "add");
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Backdrop>
+  );
+}
