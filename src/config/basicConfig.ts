598fc7c1fa6d9e0fbf7b68305e0b8a49a86fff46
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-01-17 22:29:08 +0800

    调整配置

--- src/config/basicConfig.ts
+++ src/config/basicConfig.ts
@@ -9,19 +9,29 @@ function setLayout(): void {
   const toLowerCaseUserAgent: string = navigator.userAgent.toLowerCase();
   if (
     toLowerCaseUserAgent.includes("iphone") ||
-    toLowerCaseUserAgent.includes("android")
+    toLowerCaseUserAgent.includes("android") ||
+    toLowerCaseUserAgent.includes("ipad") ||
+    toLowerCaseUserAgent.includes("mobile")
   ) {
     setBasicLayoutUnit(import.meta.env.VITE_MOBILE_VIEWPORT_UNIT);
   } else {
-    setBasicLayoutUnit(import.meta.env.VITE_PC_VIEWPORT_UNIT);
+    const clientWidth: number = document.body.clientWidth;
+    const configWidth: number = Number.parseInt(
+      import.meta.env.VITE_PC_VIEWPORT_UNIT
+    );
+
+    if (clientWidth >= configWidth) {
+      setBasicLayoutUnit(configWidth + (clientWidth - configWidth));
+    } else {
+      setBasicLayoutUnit(document.body.clientWidth * 1.4);
+    }
   }
 }
 
 // 设置初始状态
 setLayout();
-
 // 监听窗口大小改变
 window.visualViewport?.addEventListener(
   "resize",
-  CommonToolCollection.debounce(setLayout, 500, true)
+  CommonToolCollection.debounce(setLayout, 500, false)
 );
